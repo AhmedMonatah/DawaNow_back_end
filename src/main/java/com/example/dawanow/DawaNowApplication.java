0387ebe2541aa3762d5d80674301f872640b6bff
Yousef <yousefabdallah031@gmail.com>
2026-07-13 19:22:34 +0300

    Initial commit

---
+++ src/main/java/com/example/dawanow/DawaNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.dawanow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawaNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawaNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dawanow/DawaNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dawanow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawaNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
