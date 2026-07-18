fb519140c31d886bec4110ccd17884abf8e6eb3b
Sarah Sameh <sarahsameh7@gmail.com>
2026-07-19 02:12:41 +0300

    Add PharmacyAssignmentRepository

---
+++ src/main/java/com/example/dawanow/repo/PharmacyAssignmentRepository.java
@@ -0,0 +1,8 @@
+package com.example.dawanow.repo;
+
+import com.example.dawanow.entity.PharmacyAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PharmacyAssignmentRepository extends JpaRepository<PharmacyAssignment,Long> {
+}
