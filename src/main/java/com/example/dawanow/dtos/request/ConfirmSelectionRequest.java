0838a9bbaf27aa578dd39dc738147678508df3e8
Sarah Sameh <sarahsameh7@gmail.com>
2026-08-03 00:50:51 +0300

    Add FulfillmentMethod enum and include it in ConfirmSelectionRequest

--- src/main/java/com/example/dawanow/dtos/request/ConfirmSelectionRequest.java
+++ src/main/java/com/example/dawanow/dtos/request/ConfirmSelectionRequest.java
@@ -1,9 +1,10 @@
 package com.example.dawanow.dtos.request;
 
+import com.example.dawanow.entity.FulfillmentMethod;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record ConfirmSelectionRequest(
-        @NotEmpty List<Long> selectedRequestItemIds
-) {
-}
+        @NotEmpty List<Long> selectedRequestItemIds,
+        FulfillmentMethod fulfillmentMethod
+){}
